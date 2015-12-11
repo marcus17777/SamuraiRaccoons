e6897723d026e8c1390ebf8326f7848193cbe1fb
CORRU <corneliaefros@gmail.com>
2015-12-11 22:28:04 +0200

    testkood

---
+++ testtest/testtest/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace testtest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() {
+            InitializeComponent();
+            taskList.Add(new Tasks("tere"));
+
+            listView.ItemsSource = taskList;
+        }
+
+        ObservableCollection<Tasks> taskList = new ObservableCollection<Tasks>();
+        
+                
+
+        public void on_Click(object sender, RoutedEventArgs e)
+        {
+            
+            //button.Visibility = System.Windows.Visibility.Hidden;
+            taskList.Add(new Tasks("tere"));
+            
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+
+}
---
+++ testtest/testtest/Tasks.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testtest
+{
+    public class Tasks
+    {
+       public String taskName;
+
+        public Tasks(String taskName)
+        {
+            this.taskName = taskName;
+        }
+    }
+}
