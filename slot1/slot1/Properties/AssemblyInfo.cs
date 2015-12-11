c1457cf21a32be8b02b23ebf375f3f85e8a9691c
Markus Peterson <marcus17777@gmail.com>
2015-12-11 22:21:17 +0200

    .

--- slot1/slot1/Properties/AssemblyInfo.cs
+++ slot1/slot1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UWP slot1")]
+[assembly: AssemblyTitle("slot1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UWP slot1")]
+[assembly: AssemblyProduct("slot1")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
